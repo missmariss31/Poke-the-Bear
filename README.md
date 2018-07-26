4ceb545613fed55c27f7b468b08ef4e9c4d4c5d0
Marissa Schmucker <marissashcmucker@gmail.com>
2018-07-25 19:35:07 -0500

    add mad

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 ## Fun with Javascript
 ![bear](img/bear.png)
 
-This is just a fun project using Javascript querySelector, onClick, and alert.  It's a work in progress.
+This is just a fun project using Javascript querySelector, onClick, and alert.  It's a work in progress.  Poke Bob the Bear more than once to see what he has to say about it :)
--- js/poke.js
+++ js/poke.js
@@ -1,16 +1,30 @@
-let bear = document.querySelector('img');
-let feeling = document.querySelector('.feeling');
+const bear = document.querySelector('img');
+const feeling = document.querySelector('.feeling');
+const node = document.createElement('h4');
+const mad = document.createTextNode('is feeling mad');
+const happy = document.createTextNode('is feeling happy!');
+const sad = document.createTextNode('is feeling sad.');
 
-bear.onclick = function() {
-
+bear.onclick = function () {
+  let mySrc = bear.getAttribute('src');
+  /*Change Bob's facial expression to mad*/
+  if (mySrc === 'img/bear.png') {
+    bear.setAttribute('src', 'img/mad-bear.png');
+    node.appendChild(mad);
+    feeling.appendChild(node);
+  }
+  if (mySrc === 'img/happy-bear.png') {
+    bear.setAttribute('src', 'img/mad-bear.png');
+    node.removeChild(happy);
+    node.appendChild(mad);
+    feeling.appendChild(node);
+  }
+  /*Change Bob's facial expression to happy*/
+  if (mySrc === 'img/mad-bear.png') {
     alert('Ouch! Stop poking me!');
-
-    let mySrc = bear.getAttribute('src');
-
-    if(mySrc === 'img/bear.png') {
-      bear.setAttribute ('src','img/happy-bear.png');
-      
-    } else {
-      bear.setAttribute ('src','img/bear.png');
-    }
+    bear.setAttribute('src', 'img/happy-bear.png');
+    node.removeChild(mad);
+    node.appendChild(happy);
+    feeling.appendChild(node);
+  }
 }
